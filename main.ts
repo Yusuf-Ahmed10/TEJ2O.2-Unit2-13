1867e81dd5c278ec8f5480d80c79c6344c06be3c
Yusuf-Ahmed10 <yusuf.ahmed1@stu.ocsb.ca>
2026-04-08 12:42:26 -0400

    Changed and committed

--- main.ts
+++ main.ts
@@ -1,8 +1,43 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Yusuf Ahmed
+ * Created on: Apr 2026
+ * This program counts down from 4 with neopixles
 */
 
-basic.showString('Hello, World!')
+let countdown = 0
+input.onButtonPressed(Button.A, function () {
+   
+    // Start at 4
+  
+    countdown = 4
+
+    // Loop as long as countdown is 0 or higher
+   
+    while (countdown >= 0) {
+       
+        // Clear previous lights so they don't stack
+       
+        robotbit.rgb().clear()
+    
+        // Show the number on the micro:bit LED grid
+        
+        basic.showNumber(countdown)
+        
+        // Light up the number of pixels matching the countdown value
+        
+        robotbit.rgb().range(0, countdown).showColor(neopixel.colors(NeoPixelColors.Red))
+        
+        // Wait 1 second
+       
+        basic.pause(1000)
+        
+        // Decrease the countdown by 1
+        
+        countdown += -1
+    }
+    // Clean up when finished
+    
+    basic.clearScreen()
+    robotbit.rgb().clear()
+})
